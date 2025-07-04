644d8682901857042a04b4173f729791d74f6f43
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-07-04 15:10:16 +0530

    rest

--- src/main/java/org/example/ee/rest/UserResource.java
+++ src/main/java/org/example/ee/rest/UserResource.java
@@ -1,13 +1,12 @@
 package org.example.ee.rest;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 @Path("/users")
 public class UserResource {
@@ -22,5 +21,39 @@ public class UserResource {
     @Produces(MediaType.TEXT_HTML)
     public String getUser(@PathParam("id") String id){
         return id;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getUser(JsonObject jo){
+        System.out.println(jo.getString("name"));
+
+        return "ok";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getUser(User user){
+        System.out.println(user.getEmail());
+
+        return "ok";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getUser(Map user, @QueryParam("type") String type){
+
+        return "ok";
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getUser(@MatrixParam("type") Integer type){
+
+        return "ok";
     }
 }

b3a3cb640acc06e190c19afc73e5ab0c1b9f95b5
Pasindu Jayasundara <pasindujayasundara28@gmail.com>
2025-07-05 21:52:47 +0530

    core

--- web/src/main/java/org/example/ee/web/servlet/Register.java
+++ web/src/main/java/org/example/ee/web/servlet/Register.java
@@ -1,15 +1,20 @@
 package org.example.ee.web.servlet;
 
+import jakarta.ejb.EJB;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.example.ee.ejb.remote.UserSession;
 
 import java.io.IOException;
 
 @WebServlet("/register")
 public class Register extends HttpServlet {
+
+    @EJB
+    private UserSession userSession;
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
